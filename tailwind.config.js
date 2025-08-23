2341a60eba7f03dfa923c651af61db24de2730cc
Freddy Hong <wongee.hong@cooper.edu>
2025-08-23 14:28:27 -0400

    pfizer color theme added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pfizer-blue-1": "#15144B",
+        "pfizer-blue-2": "#272C77",
+        "pfizer-blue-3": "#33429D",
+        "pfizer-blue-4": "#3857A6",
+        "pfizer-blue-5": "#488CCA",
+        "pfizer-blue-6": "#74BBE6",
+        "pfizer-blue-7": "#CFEAFB",
+        "pfizer-blue-8": "#EBF5FC",
+      },
+    },
   },
   plugins: [],
 };
